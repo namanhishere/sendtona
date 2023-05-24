9a8d2d5a2940214de1de89752ab087d3696428e9
namanhishere <53372786+namanhishere@users.noreply.github.com>
2023-05-24 12:50:26 +0700

    Code init

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+const express = require("express")
+const path = require('path')
+const { EmbedBuilder, WebhookClient } = require('discord.js');
+
+const webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK });
+const app = express()
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.post('/api/post', (req, res) => {
+    const embed = new EmbedBuilder()
+        .addFields({name:"Lời nhắn đến từ: "+req.body.name,value:req.body.message })
+        .setTimestamp()
+        .setColor(0x00FFFF);
+    
+        webhookClient.send({
+            username: '#sentona',
+            avatarURL: 'https://i.pinimg.com/564x/f9/d1/0b/f9d10ba4c472d11c2506dfc4a6e895ed.jpg',
+            embeds: [embed],
+        });
+    console.log(req.body)
+})
+
+app.listen(3000)
